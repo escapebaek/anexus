782ee2c509f19f21022df2853b2d298b70a52bcb
escapeBaek <luminsishs@naver.com>
2025-07-28 23:26:20 +0900

    dp-anesthesiatrends

--- land/views.py
+++ land/views.py
@@ -11,13 +11,13 @@ def home(request):
         {"title": "Board", "text": "Join discussions and share knowledge.", "url": reverse('board_index'), "icon": "fas fa-comments"},
         {"title": "Anes Chat", "text": "Chat with other anesthesiologists.", "url": reverse('lobby'), "icon": "fas fa-comments"},
         {"title": "Drug Dictionary", "text": "Find the latest drug information.", "url": reverse('drugdictionary:drug_info'), "icon": "fas fa-pills"},
+        {"title": "Trends in Anesthesia", "text": "Recents anesthesia trends in major journals.", "url": "https://escapebaek.github.io/trends_anesthesia/", "icon": "fas fa-link"},
         {"title": "SNUH Anesthesia", "text": "More information for alumni.", "url": "https://dept.snuh.org/dept/AN/index.do", "icon": "fas fa-hospital"},
         {"title": "KSA", "text": "Korean Society of Anesthesiologists.", "url": "https://www.anesthesia.or.kr/", "icon": "fas fa-user-md"},
         {"title": "NYSORA", "text": "World-wide renowned educational organization with focus in anesthesiology.", "url": "https://www.nysora.com/", "icon": "fas fa-book-medical"},
         {"title": "OrphanAnesthesia", "text": "Anesthesia care for rare diseases.", "url": "https://www.orphananesthesia.eu/en/rare-diseases/published-guidelines.html", "icon": "fas fa-dna"},
         {"title": "Virtual TEE", "text": "Virtual TEE for education.", "url": "https://pie.med.utoronto.ca/TEE/TEE_content/TEE_standardViews_intro.html", "icon": "fas fa-heartbeat"},
         {"title": "Virtual FOB", "text": "Virtual FOB for education.", "url": "https://pie.med.utoronto.ca/VB/VB_content/simulation.html", "icon": "fas fa-lungs"},
-        {"title": "ACCRAC", "text": "Podcast for board examination.", "url": "https://accrac.com/", "icon": "fas fa-podcast"},
-        {"title": "Notion test", "text": "Testing for notion link.", "url": "https://escapebaek.notion.site/Brachial-Plexus-Block-1bc4d01a2aa648f09917278463b89ce2?pvs=25", "icon": "fas fa-link"}
+        {"title": "ACCRAC", "text": "Podcast for board examination.", "url": "https://accrac.com/", "icon": "fas fa-podcast"}
     ]
     return render(request, 'land/home.html', {'cards': cards})
