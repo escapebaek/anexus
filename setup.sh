b748f4c3c0445e944d61137179c96338d87b815d
escapeBaek <luminsishs@naver.com>
2025-08-28 17:34:54 +0900

    dp-chatFix10

--- setup.sh
+++ setup.sh
@@ -1,17 +1,10 @@
-# 에러 발생 시 즉시 중단
-set -e
+#!/bin/bash
 
-# 1. Node.js 의존성 설치 및 SASS -> CSS 컴파일
-echo "Installing Node.js dependencies and building CSS..."
-npm install
-npm run build
-
-# 2. Python 의존성 설치
-echo "Installing Python dependencies..."
+# install dependencies
+pip install setuptools
 pip install -r requirements.txt
 
-# 3. Django static 파일 수집
-echo "Collecting static files..."
+# Run django commnads
+python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --noinput
-
-echo "Build finished successfully."
