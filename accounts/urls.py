d5b318a946881d7805570997bca9ca944ea7e434
escapebaek <escapebaek@gmail.com>
2025-09-13 20:05:41 +0900

    dp-passwordResetAdd

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import login_view, register, mypage, update_user_info
-from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import (
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from django.urls import reverse_lazy
 
 urlpatterns = [
@@ -14,6 +22,23 @@ urlpatterns = [
     path('password-change-done/', PasswordChangeDoneView.as_view(
         template_name='accounts/password_change_done.html'
     ), name='password_change_done'),
+    # Password reset (custom templates matching site style)
+    path('password_reset/', PasswordResetView.as_view(
+        template_name='accounts/password_reset_form.html',
+        email_template_name='accounts/password_reset_email.html',
+        subject_template_name='accounts/password_reset_subject.txt',
+        success_url=reverse_lazy('password_reset_done')
+    ), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html',
+        success_url=reverse_lazy('password_reset_complete')
+    ), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
     path('mypage/', mypage, name='mypage'),
     path('mypage/update/', update_user_info, name='update_user_info'),
 ]
