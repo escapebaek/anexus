bc0a80015b05da567273c4f9c68f1a9ab97c0acf
escapebaek <escapebaek@gmail.com>
2025-09-14 22:06:08 +0900

    dp-landingOrderChange

--- land/views.py
+++ land/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, reverse
 
 def home(request):
     cards = [
-        {"title": "Coagulation Guideline", "text": "Find the latest coagulation guidelines.", "url": reverse('coag_index'), "icon": "fas fa-vial"},
+        {"title": "Board", "text": "Join discussions and share knowledge.", "url": reverse('board_index'), "icon": "fas fa-comments"},
         {"title": "Drug Calculator", "text": "Accurate drug dosage calculations.", "url": "https://escapebaek.github.io/anesthesia-calculator/", "icon": "fas fa-calculator"},
         {"title": "Pediatric Calculator", "text": "Accurate calculations for pediatric anesthesia.", "url": "https://escapebaek.github.io/pediatric-anesthesia-calculator/", "icon": "fas fa-baby"},
+        {"title": "Coagulation Guideline", "text": "Find the latest coagulation guidelines.", "url": reverse('coag_index'), "icon": "fas fa-vial"},
         {"title": "Questions", "text": "More information about board exam.", "url": reverse('question_home'), "icon": "fas fa-question-circle"},
         {"title": "Schedule", "text": "Surgery schedule for today.", "url": reverse('schedule_dashboard'), "icon": "fas fa-calendar-alt"},
-        {"title": "Record", "text": "Anesthesia record for today.", "url": reverse('anesthesia_record'), "icon": "fas fa-pencil-alt"},
-        {"title": "Board", "text": "Join discussions and share knowledge.", "url": reverse('board_index'), "icon": "fas fa-comments"},
         {"title": "Anes Chat", "text": "Chat with other anesthesiologists.", "url": "https://escapebaek.github.io/chat/", "icon": "fas fa-comments"},
+        {"title": "Record", "text": "Anesthesia record for today.", "url": reverse('anesthesia_record'), "icon": "fas fa-pencil-alt"},
         {"title": "Drug Dictionary", "text": "Find the latest drug information.", "url": reverse('drugdictionary:drug_info'), "icon": "fas fa-pills"},
         {"title": "Trends in Anesthesia", "text": "Recents anesthesia trends in major journals.", "url": "https://escapebaek.github.io/trends_anesthesia/", "icon": "fas fa-link"},
         {"title": "SNUH Anesthesia", "text": "More information for alumni.", "url": "https://dept.snuh.org/dept/AN/index.do", "icon": "fas fa-hospital"},
