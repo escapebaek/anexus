fd245716bd7182da53c9bbeb28cce402f305185b
escapebaek <escapebaek@gmail.com>
2025-09-14 21:40:25 +0900

    dp-examMajorFix

--- exam/urls.py
+++ exam/urls.py
@@ -8,6 +8,13 @@ urlpatterns = [
     path('<int:exam_id>/questions/', views.question_list, name='question_list'),
     path('save_exam_results/', save_exam_results, name='save_exam_results'),
     path('exam_results/', exam_results, name='exam_results'),
+    path('question/<int:question_id>/partial/', views.question_detail_partial, name='question_detail_partial'),
+    # results history and analytics
+    path('my_results/', views.my_results, name='my_results'),
+    path('analytics/exam/<int:exam_id>/', views.exam_analytics, name='exam_analytics'),
+    path('analytics/overview/', views.analytics_overview, name='analytics_overview'),
+    path('analytics/result/<int:result_id>/', views.result_analytics, name='result_analytics'),
+    path('result/<int:result_id>/delete/', views.delete_result, name='delete_result'),
     # category - 수정된 부분
     path('categories/', views.category_list, name='category_list'),
     re_path(r'^categories/(?P<category_name>[\w\s\(\)가-힣%-]+)/$', views.category_questions, name='category_questions'),
