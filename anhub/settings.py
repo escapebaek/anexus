c24d81c3e6c03d5fda320c52b0e202c3efd21aa7
escapebaek <escapebaek@gmail.com>
2025-09-22 08:58:16 +0900

    dp-domainChange

--- anhub/settings.py
+++ anhub/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'www.anexus.site', 'anexus.site', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', 'www.anexus.cloud', 'anexus.cloud', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
