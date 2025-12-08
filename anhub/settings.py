9d46c3d301efb2c287067ae4d7e34e95b82267cf
escapebaek <escapebaek@gmail.com>
2025-12-08 10:48:00 +0900

    dp-stable-debugFalse

--- anhub/settings.py
+++ anhub/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'www.anexus.cloud', 'anexus.cloud', '127.0.0.1']
 
